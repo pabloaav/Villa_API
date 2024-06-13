d26474395652bc2ecf2c5e8caf17a01b3ee07f29
pabloaav <75311490+pablovicentin@users.noreply.github.com>
2024-06-13 12:48:19 -0300

    creando primer controller api

---
+++ Villa_API/Controllers/VillaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Villa_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+    }
+}
