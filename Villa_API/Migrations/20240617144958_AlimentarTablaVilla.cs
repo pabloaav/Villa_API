e4d6f0d6ee6577cce7145f5aa2858b1922601c9d
pabloaav <pablovicentin84@gmail.com>
2024-06-17 11:50:57 -0300

    seeder con EFC

---
+++ Villa_API/Migrations/20240617144958_AlimentarTablaVilla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Villa_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: -1);
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Created_at", "Detalle", "ImagenUrl", "MetrosCuadrados", "Name", "Ocupantes", "Tarifa", "Updated_at" },
+                values: new object[] { 1, "Piscina, Wi-Fi, Aire acondicionado", new DateTime(2024, 6, 17, 11, 49, 57, 945, DateTimeKind.Local).AddTicks(9358), "Una hermosa villa con vista al mar", "http://ejemplo.com/imagen.jpg", 150, "Villa Maravilla", 4, 300.5, new DateTime(2024, 6, 17, 11, 49, 57, 945, DateTimeKind.Local).AddTicks(9373) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Created_at", "Detalle", "ImagenUrl", "MetrosCuadrados", "Name", "Ocupantes", "Tarifa", "Updated_at" },
+                values: new object[] { -1, "Piscina, Wi-Fi, Aire acondicionado", new DateTime(2024, 6, 17, 11, 40, 51, 852, DateTimeKind.Local).AddTicks(9064), "Una hermosa villa con vista al mar", "http://ejemplo.com/imagen.jpg", 150, "Villa Maravilla", 4, 300.5, new DateTime(2024, 6, 17, 11, 40, 51, 852, DateTimeKind.Local).AddTicks(9078) });
+        }
+    }
+}
--- Villa_API/Store/ApplicationDbContext.cs
+++ Villa_API/Store/ApplicationDbContext.cs
@@ -7,10 +7,28 @@ namespace Villa_API.Store
    {
       // inyeccion de dependencia en el constructor
       public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-      {
-
-      }
+      { }
       // indicamos que se cree una tabla en base de datos siguiendo el Modelo de la clase Villa
       DbSet<Villa> Villas { get; set; }
+
+      // Un seeder para cargar datos en lla tabla villas
+      protected override void OnModelCreating(ModelBuilder modelBuilder)
+      {
+         modelBuilder.Entity<Villa>().HasData(
+            new Villa()
+            {
+               Id = 1,
+               Name = "Villa Maravilla",
+               Detalle = "Una hermosa villa con vista al mar",
+               Tarifa = 300.50,
+               Ocupantes = 4,
+               MetrosCuadrados = 150,
+               ImagenUrl = "http://ejemplo.com/imagen.jpg",
+               Amenidad = "Piscina, Wi-Fi, Aire acondicionado",
+               Created_at = DateTime.Now,
+               Updated_at = DateTime.Now
+            }
+            );
+      }
    }
 }
