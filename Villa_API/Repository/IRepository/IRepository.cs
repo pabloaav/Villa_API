8d47bb1a1b65a29802f7a0dccf78d329c29c8ca7
pabloaav <pablovicentin84@gmail.com>
2024-06-19 18:32:01 -0300

    Interface Repository

---
+++ Villa_API/Repository/IRepository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Villa_API.Repository.IRepository
+{
+   // Un IRepository es una interface generica, que recibe cualquier tipo de entidad
+   public interface IRepository<T> where T : class
+   {
+      Task Create(T entity);
+      // si no se envia un filtro, devuelve toda la lista. Sino, filtra segun la funcion
+      Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null);
+      Task<T> Get(Expression<Func<T, bool>> filter, bool tracked = true);
+      Task Remove(T entity);
+      // se encarga de SaveChanges de DbContext en la base de datos
+      Task Save();
+   }
+
+
+}
---
+++ Villa_API/Repository/IRepository/Repository.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Villa_API.Store;
+
+namespace Villa_API.Repository.IRepository
+{
+   // es la clase que implementa el IRepository
+   public class Repository<T> : IRepository<T> where T : class
+   {
+      private readonly ApplicationDbContext _db;
+      internal DbSet<T> dbSet; // no hace falta el guion bajo
+      public Repository(ApplicationDbContext db)
+      {
+         _db = db;
+         dbSet = _db.Set<T>();
+      }
+      public async Task Create(T entity)
+      {
+         await dbSet.AddAsync(entity);
+         await Save();
+      }
+
+      public async Task Save()
+      {
+         await _db.SaveChangesAsync();
+      }
+
+      public async Task<T> Get(Expression<Func<T, bool>> filter, bool tracked = true)
+      {
+         IQueryable<T> query = dbSet;
+
+         query = tracked ? query : query.AsNoTracking();
+
+         if (filter != null)
+         {
+            query = query.Where(filter);
+         }
+
+         return await query.FirstOrDefaultAsync();
+      }
+
+      public async Task<List<T>> GetAll(Expression<Func<T, bool>>? filter = null)
+      {
+         IQueryable<T> query = dbSet;
+
+         if (filter != null)
+         {
+            query = query.Where(filter);
+         }
+
+         return await query.ToListAsync();
+      }
+
+      public Task Remove(T entity)
+      {
+         dbSet.Remove(entity);
+         return Save(); // aqui adentro se graban los cambios en la base de datos
+      }
+
+
+   }
+}
